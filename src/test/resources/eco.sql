9cdf4eec03b095cd5b02e052d76a2b9b8dc27b55
SXD <1512974937@qq.com>
2018-04-19 15:12:00 +0800

    修改sql

--- src/test/resources/eco.sql
+++ src/test/resources/eco.sql
@@ -20,11 +20,11 @@ SET FOREIGN_KEY_CHECKS=0;
 -- ----------------------------
 DROP TABLE IF EXISTS `account`;
 CREATE TABLE `account` (
-  `account_id` int(10) NOT NULL,
-  `account_password` varchar(18) NOT NULL,
-  `account_role_id` int(10) NOT NULL,
-  `account_role` varchar(2) NOT NULL,
-  PRIMARY KEY  (`account_id`)
+  `id` int(10) NOT NULL,
+  `password` varchar(18) NOT NULL,
+  `roleid` int(10) NOT NULL,
+  `role` varchar(2) NOT NULL,
+  PRIMARY KEY  (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
@@ -33,9 +33,9 @@ CREATE TABLE `account` (
 -- ----------------------------
 DROP TABLE IF EXISTS `system_account`;
 CREATE TABLE `system_account` (
-  `sys_id` int(11) NOT NULL,
-  `sys_password` varchar(18) NOT NULL,
-  PRIMARY KEY  (`sys_id`)
+  `id` int(11) NOT NULL,
+  `password` varchar(18) NOT NULL,
+  PRIMARY KEY  (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
@@ -44,13 +44,13 @@ CREATE TABLE `system_account` (
 -- ----------------------------
 DROP TABLE IF EXISTS `course`;
 CREATE TABLE `course` (
-  `course_id` int(11) NOT NULL,
-  `course_name` varchar(15) NOT NULL,
-  `course_info` varchar(50) NOT NULL,
-  `course_type` varchar(10) NOT NULL,
-  `course_price` float(10,2) NOT NULL,
-  `course_img_url` varchar(50) default NULL,
-  PRIMARY KEY  (`course_id`)
+  `courseid` int(11) NOT NULL,
+  `coursename` varchar(15) NOT NULL,
+  `info` varchar(50) NOT NULL,
+  `type` varchar(10) NOT NULL,
+  `price` float(10,2) NOT NULL,
+  `imgurl` varchar(50) default NULL,
+  PRIMARY KEY  (`courseid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
@@ -58,15 +58,14 @@ CREATE TABLE `course` (
 -- ----------------------------
 DROP TABLE IF EXISTS `course_record`;
 CREATE TABLE `course_record` (
-  `course_record_id` int(11) NOT NULL,
-  `course_id` int(11) NOT NULL,
-  `course_start_time` datetime default NULL,
-  `course_end_time` datetime default NULL,
-  `course_close_time` datetime default NULL,
-  `course_sign_count` int(11) default '0',
-  PRIMARY KEY  (`course_record_id`),
-  KEY `fk_course_id` (`course_id`),
-  CONSTRAINT `fk_course_id` FOREIGN KEY (`course_id`) REFERENCES `course_record` (`course_id`)
+  `courserecordid` int(11) NOT NULL,
+  `courseid` int(11) NOT NULL,
+  `starttime` datetime default NULL,
+  `endtime` datetime default NULL,
+  `closetime` datetime default NULL,
+  `signcount` int(11) default '0',
+  PRIMARY KEY  (`courserecordid`),
+  CONSTRAINT `fk_course_id` FOREIGN KEY (`courseid`) REFERENCES `course` (`courseid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
@@ -78,13 +77,13 @@ CREATE TABLE `course_record` (
 -- ----------------------------
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
-  `user_id` int(11) NOT NULL auto_increment,
-  `user_name` varchar(10) NOT NULL,
-  `user_age` int(11) default NULL,
-  `user_id_card` varchar(18) NOT NULL,
-  `user_sex` varchar(5) default NULL,
-  `user_img_url` varchar(50) default NULL,
-  PRIMARY KEY  (`user_id`)
+  `userid` int(11) NOT NULL auto_increment,
+  `username` varchar(10) NOT NULL,
+  `age` int(11) default NULL,
+  `idcard` varchar(18) NOT NULL,
+  `sex` varchar(5) default NULL,
+  `imgurl` varchar(50) default NULL,
+  PRIMARY KEY  (`userid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
@@ -92,15 +91,15 @@ CREATE TABLE `user` (
 -- ----------------------------
 DROP TABLE IF EXISTS `teacher`;
 CREATE TABLE `teacher` (
-  `teacher_id` int(11) NOT NULL,
-  `teacher_id_card` varchar(18) NOT NULL,
-  `teacher_name` varchar(10) NOT NULL,
-  `teacher_sex` varchar(5) default NULL,
-  `teacher_age` int(11) default NULL,
-  `teacher_phone` varchar(13) NOT NULL,
-  `teacher_address` varchar(50) NOT NULL,
-  `teacher_img_url` varchar(50) default NULL,
-  PRIMARY KEY  (`teacher_id`)
+  `teacherid` int(11) NOT NULL,
+  `idcard` varchar(18) NOT NULL,
+  `name` varchar(10) NOT NULL,
+  `sex` varchar(5) default NULL,
+  `age` int(11) default NULL,
+  `phone` varchar(13) NOT NULL,
+  `address` varchar(50) NOT NULL,
+  `imgurl` varchar(50) default NULL,
+  PRIMARY KEY  (`teacherid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
@@ -109,17 +108,15 @@ CREATE TABLE `teacher` (
 -- ----------------------------
 DROP TABLE IF EXISTS `class`;
 CREATE TABLE `class` (
-  `class_id` int(11) NOT NULL,
-  `teacher_id` int(11) NOT NULL,
-  `course_record_id` int(11) NOT NULL,
-  `class_name` varchar(50) default NULL,
-  `student_count` varchar(50) default NULL,
+  `classid` int(11) NOT NULL,
+  `teacherid` int(11) NOT NULL,
+  `courserecordid` int(11) NOT NULL,
+  `classname` varchar(50) default NULL,
+  `usercount` int(11) default NULL,
   `classroom` varchar(50) default NULL,
-  PRIMARY KEY  (`class_id`),
-  KEY `fk_teacher_id` (`teacher_id`),
-  KEY `fk_course_record_id` (`course_record_id`),
-  CONSTRAINT `fk_course_record_id` FOREIGN KEY (`course_record_id`) REFERENCES `course_record` (`course_record_id`),
-  CONSTRAINT `fk_teacher_id` FOREIGN KEY (`teacher_id`) REFERENCES `teacher` (`teacher_id`)
+  PRIMARY KEY  (`classid`),
+  CONSTRAINT `fk_course_record_id` FOREIGN KEY (`courserecordid`) REFERENCES `course_record` (`courserecordid`),
+  CONSTRAINT `fk_teacher_id` FOREIGN KEY (`teacherid`) REFERENCES `teacher` (`teacherid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
@@ -128,13 +125,12 @@ CREATE TABLE `class` (
 -- ----------------------------
 DROP TABLE IF EXISTS `user_class`;
 CREATE TABLE `user_class` (
-  `user_id` int(11) NOT NULL,
-  `class_id` int(11) NOT NULL,
-  `is_ assigned` int(2) NOT NULL,
-  PRIMARY KEY  (`user_id`,`class_id`),
-  KEY `fk_class_id` (`class_id`),
-  CONSTRAINT `fk_user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`),
-  CONSTRAINT `fk_class_id` FOREIGN KEY (`class_id`) REFERENCES `class` (`class_id`)
+  `userid` int(11) NOT NULL,
+  `classid` int(11) NOT NULL,
+  `isassigned` int(2) NOT NULL,
+  PRIMARY KEY  (`userid`,`classid`),
+  CONSTRAINT `fk_user_id` FOREIGN KEY (`userid`) REFERENCES `user` (`userid`),
+  CONSTRAINT `fk_class_id` FOREIGN KEY (`classid`) REFERENCES `class` (`classid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
@@ -142,19 +138,16 @@ CREATE TABLE `user_class` (
 -- ----------------------------
 DROP TABLE IF EXISTS `teacher_back_info`;
 CREATE TABLE `teacher_back_info` (
-  `teacher_back_id` int(11) NOT NULL,
-  `teacher_id` int(11) default NULL,
-  `user_id` int(11) default NULL,
-  `class_id` int(11) default NULL,
-  `back_time` datetime default NULL,
-  `back_info` varchar(50) default NULL,
-  PRIMARY KEY  (`teacher_back_id`),
-  KEY `fk1_teacher_id` (`teacher_id`),
-  KEY `fk1_user_id` (`user_id`),
-  KEY `fk1_class_id` (`class_id`),
-  CONSTRAINT `fk1_class_id` FOREIGN KEY (`class_id`) REFERENCES `class` (`class_id`),
-  CONSTRAINT `fk1_teacher_id` FOREIGN KEY (`teacher_id`) REFERENCES `teacher` (`teacher_id`),
-  CONSTRAINT `fk1_user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`)
+  `teacherbackid` int(11) NOT NULL,
+  `teacherid` int(11) default NULL,
+  `userid` int(11) default NULL,
+  `classid` int(11) default NULL,
+  `backtime` datetime default NULL,
+  `backinfo` varchar(50) default NULL,
+  PRIMARY KEY  (`teacherbackid`),
+  CONSTRAINT `fk1_class_id` FOREIGN KEY (`classid`) REFERENCES `class` (`classid`),
+  CONSTRAINT `fk1_teacher_id` FOREIGN KEY (`teacherid`) REFERENCES `teacher` (`teacherid`),
+  CONSTRAINT `fk1_user_id` FOREIGN KEY (`userid`) REFERENCES `user` (`userid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
@@ -164,19 +157,16 @@ CREATE TABLE `teacher_back_info` (
 -- ----------------------------
 DROP TABLE IF EXISTS `user_back_info`;
 CREATE TABLE `user_back_info` (
-  `user_back_id` int(11) NOT NULL,
-  `teacher_id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL,
-  `class_id` int(11) NOT NULL,
-  `back_time` datetime default NULL,
-  `back_info` varchar(50) default NULL,
-  PRIMARY KEY  (`user_back_id`),
-  KEY `fk2_teacher_id` (`teacher_id`),
-  KEY `fk2_user_id` (`user_id`),
-  KEY `fk2_class_id` (`class_id`),
-  CONSTRAINT `fk2_class_id` FOREIGN KEY (`class_id`) REFERENCES `class` (`class_id`),
-  CONSTRAINT `fk2_teacher_id` FOREIGN KEY (`teacher_id`) REFERENCES `teacher` (`teacher_id`),
-  CONSTRAINT `fk2_user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`)
+  `userbackid` int(11) NOT NULL,
+  `teacherid` int(11) NOT NULL,
+  `userid` int(11) NOT NULL,
+  `classid` int(11) NOT NULL,
+  `backtime` datetime default NULL,
+  `backinfo` varchar(50) default NULL,
+  PRIMARY KEY  (`userbackid`),
+  CONSTRAINT `fk2_class_id` FOREIGN KEY (`classid`) REFERENCES `class` (`classid`),
+  CONSTRAINT `fk2_teacher_id` FOREIGN KEY (`teacherid`) REFERENCES `teacher` (`teacherid`),
+  CONSTRAINT `fk2_user_id` FOREIGN KEY (`userid`) REFERENCES `user` (`userid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
@@ -184,14 +174,12 @@ CREATE TABLE `user_back_info` (
 -- ----------------------------
 DROP TABLE IF EXISTS `time_sheet`;
 CREATE TABLE `time_sheet` (
-  `time_sheet_id` int(11) NOT NULL default '0',
-  `user_id` int(11) default NULL,
-  `class_id` int(11) default NULL,
-  `record_time` datetime default NULL,
-  `sheet_info` varchar(50) default NULL,
-  PRIMARY KEY  (`time_sheet_id`),
-  KEY `fk3_user_id` (`user_id`),
-  KEY `fk3_class_id` (`class_id`),
-  CONSTRAINT `fk3_user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`),
-  CONSTRAINT `fk3_class_id` FOREIGN KEY (`class_id`) REFERENCES `class` (`class_id`)
+  `timesheetid` int(11) NOT NULL default '0',
+  `userid` int(11) default NULL,
+  `classid` int(11) default NULL,
+  `recordtime` datetime default NULL,
+  `sheetinfo` varchar(50) default NULL,
+  PRIMARY KEY  (`timesheetid`),
+  CONSTRAINT `fk3_user_id` FOREIGN KEY (`userid`) REFERENCES `user` (`userid`),
+  CONSTRAINT `fk3_class_id` FOREIGN KEY (`classid`) REFERENCES `class` (`classid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
