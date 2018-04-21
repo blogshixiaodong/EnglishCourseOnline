dfd74acb159402772df01380d93a7f2134437ef8
xxxx <www.jjmjjm@qq.com>
2018-04-21 15:06:43 +0800

    timesheetdao

--- src/main/java/com/eco/dao/TimeSheetDao.java
+++ src/main/java/com/eco/dao/TimeSheetDao.java
@@ -12,4 +12,23 @@ import com.eco.bean.model.TimeSheet;
 public interface TimeSheetDao extends BaseDao<TimeSheet> {
 	
 	public abstract List<TimeSheetDetail> getTimeSheetDetail(Integer engclass);
+	
+	
+	/** 
+	* @Description: 通过用户id和班级id,返回用户所在班级的考勤记录 
+	* @param userid
+	* @param engclass
+	* @return List<TimeSheetDao>
+	*/ 
+	public List<TimeSheetDetail> getTimeSheetByUser(Integer userid,Integer engclass);
+	
+	
+	/** 
+	* @Description: 通过考勤对象创建考勤记录 
+	* @param timeSheet
+	* @return boolean
+	*/ 
+	public boolean createTimeSheet(TimeSheet timeSheet);
+	
+	
 }
--- src/main/java/com/eco/dao/impl/TimeSheetDaoImpl.java
+++ src/main/java/com/eco/dao/impl/TimeSheetDaoImpl.java
@@ -17,6 +17,25 @@ public class TimeSheetDaoImpl extends AbstractBaseDao<TimeSheet> implements Time
 		// TODO Auto-generated method stub
 		return null;
 	}
+
+	@Override
+	public List<TimeSheetDetail> getTimeSheetByUser(Integer userid, Integer classid) {
+		String sql = "SELECT t5.username,t2.userid,t2.classid,t2.recordtime,t2.sheetinfo,t4.name,t4.teacherid,t3.classname,t3.classroom "+
+					"FROM user_class t1 LEFT JOIN time_sheet t2 ON t1.`classid` = t2.`classid` AND t1.userid = t2.userid " + 
+					"LEFT JOIN  engclass t3 ON t1.classid = t3.classid LEFT JOIN  Teacher t4 ON t3.teacherid = t4.teacherid " + 
+					"LEFT JOIN USER t5 ON t5.userid = t1.userid WHERE t1.`classid` = ? AND t1.`userid`= ?";
+		
+		List<TimeSheetDetail> timeSheetList = queryForListEx(sql, TimeSheetDetail.class, userid,classid);
+		
+		return timeSheetList;
+	}
+
+	@Override
+	public boolean createTimeSheet(TimeSheet timeSheet) {
+		String sql = "INSERT INTO time_sheet(userid,classid,recordtime,sheetinfo) VALUES(?,?,?,?)";
+		int record = insert(sql,timeSheet.getUserId(),timeSheet.getClassId(),timeSheet.getRecordTime(),timeSheet.getSheetInfo());
+		return record == 1;
+	}
 	
 	
 
