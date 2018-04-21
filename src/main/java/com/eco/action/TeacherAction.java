8bd5fa26b76335b1f8cd9fd3cfd7e7a2129566bb
SXD <1512974937@qq.com>
2018-04-21 10:22:13 +0800

    action文件

---
+++ src/main/java/com/eco/action/TeacherAction.java
@@ -0,0 +1,15 @@
+package com.eco.action;
+
+import com.eco.bean.model.Teacher;
+import com.opensymphony.xwork2.ActionSupport;
+
+/*
+ * date:   2018年4月19日 上午9:43:15
+ * author: Shixiaodong
+ */
+public class TeacherAction extends ActionSupport {
+
+	private static final long serialVersionUID = 1L;
+	
+
+}
