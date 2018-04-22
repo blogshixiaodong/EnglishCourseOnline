756b4c71bc42ed7db08289beee381a31fca24bd5
SXD <1512974937@qq.com>
2018-04-22 20:30:16 +0800

    更新server层

--- src/main/java/com/eco/server/impl/TeacherServerImpl.java
+++ src/main/java/com/eco/server/impl/TeacherServerImpl.java
@@ -3,8 +3,11 @@ package com.eco.server.impl;
 import java.util.List;
 
 import com.eco.bean.dto.CourseDetail;
+import com.eco.bean.dto.EngclassDetail;
 import com.eco.dao.CourseDao;
+import com.eco.dao.EngclassDao;
 import com.eco.dao.impl.CourseDaoImpl;
+import com.eco.dao.impl.EngclassDaoImpl;
 import com.eco.server.TeacherServer;
 
 /*
@@ -14,7 +17,7 @@ import com.eco.server.TeacherServer;
 public class TeacherServerImpl implements TeacherServer {
 
 	private CourseDao courseDao = new CourseDaoImpl();
-	
+	private EngclassDao engclassDao = new EngclassDaoImpl();
 	
 	@Override
 	public List<CourseDetail> getNowCourseDetailList(Integer teacherId) {
@@ -31,6 +34,9 @@ public class TeacherServerImpl implements TeacherServer {
 		return courseDao.getAllCourseDetailList(teacherId);
 	}
 	
+	public List<EngclassDetail> getEngclassList(Integer teacherId) {
+		return engclassDao.getEngclassList(teacherId);
+	}
 	
 	
 }
