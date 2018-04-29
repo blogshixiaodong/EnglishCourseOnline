983e8d54390cbe665ea6bcd0556a7d2792c69b45
SXD <1512974937@qq.com>
2018-04-29 18:11:18 +0800

    CourseRecordDao代码整理

--- src/main/java/com/eco/dao/CourseRecordDao.java
+++ src/main/java/com/eco/dao/CourseRecordDao.java
@@ -5,31 +5,27 @@ import java.util.List;
 import com.eco.bean.dto.CourseDetail;
 import com.eco.bean.model.CourseRecord;
 
-
-
 public interface CourseRecordDao {
 	
 	/** 
-	* @Description:创建课程记录 
+	* 创建课程记录 
 	* @param record
 	* @return boolean
 	*/ 
-	public abstract boolean  createCourseRecord(CourseRecord record);
-	
+	public abstract boolean  insert(CourseRecord record);
 	
 	/** 
-	* @Description: 获取真正等待报名的课程记录 
+	* 获取真正等待报名的课程记录 
 	* @param courseid
 	* @return List<CourseDetail>
 	*/ 
-	public abstract List<CourseDetail> queryNowCoureseRecord(Integer courseid);
-	
+	public abstract List<CourseDetail> selectNowCoureseRecordByCourseId(Integer courseid);
 	
 	/** 
-	* @Description:报名成功，修改报名人数字段
+	* 报名成功，修改报名人数字段
 	* @param crId
 	* @return boolean
 	*/ 
-	public abstract boolean updateSignCount(Integer crId);
+	public abstract boolean updateCourseRecordSignCount(Integer crId);
 	
 }
