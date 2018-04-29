a49e11b1bfe337ff4124f32ae6524099d703dd06
SXD <1512974937@qq.com>
2018-04-29 20:56:17 +0800

    TeacherServer代码整理

--- src/main/java/com/eco/server/TeacherServer.java
+++ src/main/java/com/eco/server/TeacherServer.java
@@ -12,16 +12,48 @@ import com.eco.bean.model.Engclass;
  */
 public interface TeacherServer {
 	
-	public abstract List<CourseDetail> getNowCourseDetailList(Integer teacherId);
+	/**
+	 * 根据教师id获取教师正在进行的课程列表
+	 * @param teacherId
+	 * @return
+	 */
+	public abstract List<CourseDetail> queryNowCourseDetailListByTeacherId(Integer teacherId);
 	
-	public abstract List<CourseDetail> getHistoryCourseDetailList(Integer teacherId);
+	/**
+	 * 根据教师id获取教师已完成的课程列表信息
+	 * @param teacherId
+	 * @return
+	 */
+	public abstract List<CourseDetail> queryHistoryCourseDetailListByTeacherId(Integer teacherId);
 	
-	public abstract List<CourseDetail> getAllCourseDetailList(Integer teacherId);
+	/**
+	 * 根据教师id获取教师所有的课程列表信息
+	 * @param teacherId
+	 * @return
+	 */
+	public abstract List<CourseDetail> queryAllCourseDetailListByTeacherId(Integer teacherId);
 	
-	public abstract List<EngclassDetail> getEngclassList(Integer teacherId);
+	/**
+	 * 根据教师id获取教师教过的班级详细信息
+	 * @param teacherId
+	 * @return
+	 */
+	public abstract List<EngclassDetail> queryEngclassListByTeacherId(Integer teacherId);
 
-	public List<Engclass> getEngclassByClassIdAndName(Integer teacherId, Integer classId, String className);
+	/**
+	 * 根据教师id，班级id，班级名称 获取班级基本信息
+	 * @param teacherId
+	 * @param engclassId
+	 * @param engclassName
+	 * @return
+	 */
+	public List<Engclass> queryEngclassByCondition(Integer teacherId, Integer engclassId, String engclassName);
 	
-	public EngclassDetail getEngclassDetail(Integer classId);
+	/**
+	 * 通过班级编号，获取班级详细信息
+	 * @param engclassId
+	 * @return
+	 */
+	public EngclassDetail queryEngclassDetailByEngclassId(Integer engclassId);
 	
 }
--- src/main/java/com/eco/server/impl/TeacherServerImpl.java
+++ src/main/java/com/eco/server/impl/TeacherServerImpl.java
@@ -19,29 +19,31 @@ import com.eco.server.TeacherServer;
 public class TeacherServerImpl implements TeacherServer {
 
 	private CourseDao courseDao = new CourseDaoImpl();
+	
 	private EngclassDao engclassDao = new EngclassDaoImpl();
 	
 	@Override
-	public List<CourseDetail> getNowCourseDetailList(Integer teacherId) {
+	public List<CourseDetail> queryNowCourseDetailListByTeacherId(Integer teacherId) {
 		return courseDao.selectTeacherNowCourseDetailListByTeacherId(teacherId);
 	}
 
 	@Override
-	public List<CourseDetail> getHistoryCourseDetailList(Integer teacherId) {
+	public List<CourseDetail> queryHistoryCourseDetailListByTeacherId(Integer teacherId) {
 		return courseDao.selectTeacherIdHistoryCourseDetailListByTeacherId(teacherId);
 	}
 
 	@Override
-	public List<CourseDetail> getAllCourseDetailList(Integer teacherId) {
+	public List<CourseDetail> queryAllCourseDetailListByTeacherId(Integer teacherId) {
 		return courseDao.selectAllCourseDetailListByTeacherId(teacherId);
 	}
 	
-	public List<EngclassDetail> getEngclassList(Integer teacherId) {
+	@Override
+	public List<EngclassDetail> queryEngclassListByTeacherId(Integer teacherId) {
 		return engclassDao.selectEngclassListByTeacherId(teacherId);
 	}
 
 	@Override
-	public List<Engclass> getEngclassByClassIdAndName(Integer teacherId, Integer classId, String className) {
+	public List<Engclass> queryEngclassByCondition(Integer teacherId, Integer classId, String className) {
 		List<Engclass> engclassList = null;
 		if(classId == null) {
 			if(className == null || "".equals(className)) {
@@ -57,10 +59,8 @@ public class TeacherServerImpl implements TeacherServer {
 	}
 
 	@Override
-	public EngclassDetail getEngclassDetail(Integer classId) {
+	public EngclassDetail queryEngclassDetailByEngclassId(Integer classId) {
 		return engclassDao.selectEngclassDetailByEngclassId(classId);
 	}
-	
-	
-	
+
 }
