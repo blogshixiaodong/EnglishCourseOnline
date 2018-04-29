0b9ea5074cffed35d55581fc68a8d316fa205ef4
SXD <1512974937@qq.com>
2018-04-29 17:27:27 +0800

    TeacherDao代码整理

--- src/main/java/com/eco/dao/TeacherDao.java
+++ src/main/java/com/eco/dao/TeacherDao.java
@@ -1,12 +1,6 @@
 package com.eco.dao;
 
-import java.util.Date;
-import java.util.List;
-
-import com.eco.bean.dto.CourseDetail;
-import com.eco.bean.dto.TimeSheetDetail;
 import com.eco.bean.model.Teacher;
-import com.eco.bean.model.User;
 
 /*
  * date:   2018年4月19日 上午9:45:03
@@ -14,11 +8,11 @@ import com.eco.bean.model.User;
  */
 public interface TeacherDao extends BaseDao<Teacher> {
 	
-	public abstract int insertTeacher(Teacher teacher);
+	/**
+	 * 添加教师记录
+	 * @param teacher
+	 * @return
+	 */
+	public abstract int insert(Teacher teacher);
 	
-
-	
-	
-	
-
 }
